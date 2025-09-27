bfcc61d5ded06f06feb66152b5ecb308c5c56841
JoshDominguezV <jdomiinguezz@gmail.com>
2025-09-27 17:19:12 -0600

    Rutas para vercel

--- api/server.js
+++ api/server.js
@@ -2,18 +2,38 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
-
-server.use(middlewares)
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/product/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
+const middlewares = jsonServer.defaults({
+  static: 'node_modules/json-server/public'
 })
 
-// Export the Server API
+// Habilitar CORS
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200)
+  } else {
+    next()
+  }
+})
+
+server.use(middlewares)
+
+// Configuración de rutas
+server.use(jsonServer.rewriter({
+  '/api/projects': '/projects',
+  '/api/projects/:id': '/projects/:id',
+  '/api/tasks': '/tasks',
+  '/api/tasks/:id': '/tasks/:id'
+}))
+
+server.use('/api', router)
+
+// Manejar rutas no encontradas
+server.use((req, res) => {
+  res.status(404).json({ error: 'Endpoint not found' })
+})
+
 module.exports = server
