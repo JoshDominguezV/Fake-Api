214eca60b63e5139bbbb8122c8b73e1d52d26406
JoshDominguezV <jdomiinguezz@gmail.com>
2025-09-28 12:23:28 -0600

    Memoria

--- api/server.js
+++ api/server.js
@@ -1,25 +1,34 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+
+
+const router = jsonServer.router({
+  users: [],
+  projects: [],
+  tasks: []
+});
+
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-// Add this before server.use(router)
+
+// Rewriter para tener rutas /api/...
 server.use(
   jsonServer.rewriter({
     "/api/projects": "/projects",
     "/api/projects/:id": "/projects/:id",
     "/api/tasks": "/tasks",
     "/api/tasks/:id": "/tasks/:id",
+    "/api/users": "/users",
+    "/api/users/:id": "/users/:id",
   })
 );
+
 server.use(router);
+
 server.listen(3000, () => {
-  console.log("JSON Server is running");
+  console.log("JSON Server is running with in-memory DB 🚀");
 });
 
-// Export the Server API
+
 module.exports = server;
-
-
-
